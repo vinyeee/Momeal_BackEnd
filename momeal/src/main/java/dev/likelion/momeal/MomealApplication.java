08e364b3b0aa91bf7d1fa598bf3d63d3da195ad5
yodayeong <duekdud01@naver.com>
2023-08-02 15:34:43 +0900

    feat: 모밀 프로젝트 업로드

---
+++ momeal/src/main/java/dev/likelion/momeal/MomealApplication.java
@@ -0,0 +1,13 @@
+package dev.likelion.momeal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MomealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MomealApplication.class, args);
+	}
+
+}
---
+++ momeal/src/test/java/dev/likelion/momeal/MomealApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.likelion.momeal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MomealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
