37c7da31b9d20fd194a9a18abfe0fe8e8305017c
yodayeong <duekdud01@naver.com>
2023-08-08 11:37:06 +0900

    feat: MenuDto 업로드

--- momeal/src/main/java/dev/likelion/momeal/TestComponent.java
+++ momeal/src/main/java/dev/likelion/momeal/TestComponent.java
@@ -1,5 +1,6 @@
 package dev.likelion.momeal;
 
+import dev.likelion.momeal.entity.MenuEntity;
 import dev.likelion.momeal.repository.MenuRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -9,6 +10,7 @@ public class TestComponent {
     public TestComponent(
             @Autowired MenuRepository menuRepository
             ) {
+        MenuEntity menuEntity = new MenuEntity();
 
     }
 }
--- momeal/src/main/java/dev/likelion/momeal/dto/MenuDto.java
+++ momeal/src/main/java/dev/likelion/momeal/dto/MenuDto.java
@@ -1,4 +1,71 @@
 package dev.likelion.momeal.dto;
 
 public class MenuDto {
+    private Long id;
+    private String title;
+    private int price;
+    private String picture;
+    private int amount;
+
+    public MenuDto() {
+    }
+
+    public MenuDto(Long id, String title, int price, String picture, int amount) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+        this.picture = picture;
+        this.amount = amount;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuDto{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", price=" + price +
+                ", picture='" + picture + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
